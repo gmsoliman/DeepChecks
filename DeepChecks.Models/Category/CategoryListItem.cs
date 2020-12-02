0faed2b97347af15258f43d75397872395bffeb2
gmsoliman <gmsoliman13@gmail.com>
2020-12-02 06:04:20 -0500

    added Category model/service/controller/view

---
+++ DeepChecks.Models/Category/CategoryListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepChecks.Models.Category
+{
+    public class CategoryListItem
+    {
+        [Display(Name="Category")]
+        public string CategoryTitle { get; set; }
+        [Display(Name = "Description")]
+        public string CategoryDescription { get; set; }
+    }
+}
---
+++ DeepChecks.WebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using DeepChecks.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeepChecks.WebMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var model = new CategoryListItem[0];
+            return View(model);
+        }
+    }
+}
