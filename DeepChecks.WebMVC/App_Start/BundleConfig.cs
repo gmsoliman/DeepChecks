8565b1f1c68e8ec096e077e06bf9f4e7d183d872
gmsoliman <gmsoliman13@gmail.com>
2020-12-07 01:49:06 -0500

    styled views

--- DeepChecks.WebMVC/App_Start/BundleConfig.cs
+++ DeepChecks.WebMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace DeepChecks.WebMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
